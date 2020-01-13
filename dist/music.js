28df0d1da0728cf1e3945f30c7343d732d3e68a2
Scott Yeung <yeunghl@whu.edu.cn>
2020-01-13 11:33:22 +0800

    Site updated: 2020-01-13 11:33:21

--- dist/music.js
+++ dist/music.js
@@ -4,6 +4,12 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
+        name: "芝麻开门",
+        artist: "李幸倪",
+        url: "http://111.47.194.11/amobile.music.tc.qq.com/C400001vbxMc4avrUg.m4a?guid=8029882072&vkey=3B95ADE09F9E5CFBDD9C45CE63F0B029E4C236C5EC5DCB1BFC764C359227B62A838CB1119FBDCF3D86FAC3086210429EDC103941010AA085&uin=2628&fromtag=66",
+        cover: "https://y.gtimg.cn/music/photo_new/T002R300x300M000000tbCh12RdPbg.jpg?max_age=2592000",        
+      },
+      {
         name: "披星戴月",
         artist: '张敬轩',
         url: 'http://music.163.com/song/media/outer/url?id=188376.mp3',
