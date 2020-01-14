13880e5b7acb85ff78dc99ef85aed40f62034e57
Scott Yeung <yeunghl@whu.edu.cn>
2020-01-14 15:48:25 +0800

    Site updated: 2020-01-14 15:48:24

--- dist/music.js
+++ dist/music.js
@@ -6,7 +6,7 @@ const ap = new APlayer({
       {
         name: "芝麻开门",
         artist: "李幸倪",
-        url: "http://111.47.194.11/amobile.music.tc.qq.com/C400001vbxMc4avrUg.m4a?guid=8029882072&vkey=3B95ADE09F9E5CFBDD9C45CE63F0B029E4C236C5EC5DCB1BFC764C359227B62A838CB1119FBDCF3D86FAC3086210429EDC103941010AA085&uin=2628&fromtag=66",
+        url: "http://music.163.com/song/media/outer/url?id=410338018.mp3",
         cover: "https://y.gtimg.cn/music/photo_new/T002R300x300M000000tbCh12RdPbg.jpg?max_age=2592000",        
       },
       {
@@ -15,11 +15,17 @@ const ap = new APlayer({
         url: 'http://music.163.com/song/media/outer/url?id=188376.mp3',
         cover: 'http://p1.music.126.net/WZY0jAKYiRd0tL7wyJn7zg==/38482906984309.jpg?param=130y130',
       },
+      // {//网易版权没了
+      //   name: '重新找到你',
+      //   artist: '农夫',
+      //   url: 'http://music.163.com/song/media/outer/url?id=368756.mp3',
+      //   cover: 'http://p2.music.126.net/yJ2MEAY81cKHWNFRiCeKOg==/94557999994101.jpg?param=130y130',
+      // }
       {
-        name: '重新找到你',
-        artist: '农夫',
-        url: 'http://music.163.com/song/media/outer/url?id=368756.mp3',
-        cover: 'http://p2.music.126.net/yJ2MEAY81cKHWNFRiCeKOg==/94557999994101.jpg?param=130y130',
+        name: "我歌故我在",
+        artist: '谢安琪',
+        url: 'http://music.163.com/song/media/outer/url?id=5256471.mp3',
+        cover: 'http://p2.music.126.net/i2P1ox4pWG9VLeoRFeUxfQ==/76965813961922.jpg',
       }
     ]
 });
